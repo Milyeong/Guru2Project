78cdb3c9adbc86c2436e4a1bcbdac2aca6bca552
jiseo <ellen9912@naver.com>
2021-07-27 16:41:48 +0900

    Loading - Login

---
+++ app/src/main/java/com/example/guru2project/SettingTimeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.guru2project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingTimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_time)
+    }
+}
