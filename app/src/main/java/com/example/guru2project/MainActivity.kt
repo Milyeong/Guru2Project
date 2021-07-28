5bc8acceb673c676542a9b2ac4bd240d7ab466c4
Milyeong <hon109909@gmail.com>
2021-07-28 14:47:24 +0900

    Commit : firebase 연동
    # Conflicts:
    #	.idea/compiler.xml

--- app/src/main/java/com/example/guru2project/MainActivity.kt
+++ app/src/main/java/com/example/guru2project/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.example.guru2project
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.app.AppOpsManager
+import android.app.AppOpsManager.MODE_ALLOWED
+import android.app.AppOpsManager.OPSTR_GET_USAGE_STATS
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Process.myUid
+import android.provider.Settings
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
