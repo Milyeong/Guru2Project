c07fbff45d5a3eca32e30cf656fe40431bc822db
JSA <sooa3581@gmail.com>
2021-07-28 14:14:19 +0900

    //사용정보 접근 권한(미완)

--- app/src/main/java/com/example/guru2project/MainActivity.kt
+++ app/src/main/java/com/example/guru2project/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.guru2project
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.app.AppOpsManager
+import android.app.AppOpsManager.MODE_ALLOWED
+import android.app.AppOpsManager.OPSTR_GET_USAGE_STATS
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Process.myUid
+import android.provider.Settings
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //사용정보 접근 권한이 허용되지 않았을때
+        if (!checkForPermission()) {
+            var dlg = AlertDialog.Builder(this)
+            dlg.setTitle("권한이 필요한 이유")
+            dlg.setMessage("어플 사용을 위해서 사용정보 접근 허용이 필요합니다.")
+            dlg.setPositiveButton( "확인") { dialog, which ->
+                startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+            }
+            dlg.setNegativeButton("취소", null)
+            dlg.show()
+
+        } else { //이미 권한이 허용된 경우
+            //화면 넘어감
+        }
+    }
+    // 사용정보 접근 권한 허용 여부 확인
+    @SuppressLint("NewApi")
+    private fun checkForPermission(): Boolean {
+        val appOps = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val mode = appOps.checkOpNoThrow(OPSTR_GET_USAGE_STATS, myUid(), packageName)
+        return mode == MODE_ALLOWED
     }
 }
