fb1bf796369136ab0af8f34b70d9c4efc28beaf9
Milyeong <hon109909@gmail.com>
2021-07-28 14:49:57 +0900

    Commit : firebase 연동 / 사용 권한 수정
    # Conflicts:
    #	.idea/compiler.xml

--- app/src/main/java/com/example/guru2project/MainActivity.kt
+++ app/src/main/java/com/example/guru2project/MainActivity.kt
@@ -16,5 +16,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //사용정보 접근 권한이 허용되지 않았을때
+        if (!checkForPermission()) {
+            var dlg = AlertDialog.Builder(this)
+            dlg.setTitle("권한이 필요한 이유")
+            dlg.setMessage("어플 사용을 위해서 사용정보 접근 허용이 필요합니다.")
+            dlg.setPositiveButton( "확인") { dialog, which ->
+                startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+            }
+            dlg.setNegativeButton("취소", null)
+            dlg.show()
+
+        } else { //이미 권한이 허용된 경우
+            //화면 넘어감
+        }
+    }
+    // 사용정보 접근 권한 허용 여부 확인
+    @SuppressLint("NewApi")
+    private fun checkForPermission(): Boolean {
+        val appOps = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val mode = appOps.checkOpNoThrow(OPSTR_GET_USAGE_STATS, myUid(), packageName)
+        return mode == MODE_ALLOWED
     }
 }
