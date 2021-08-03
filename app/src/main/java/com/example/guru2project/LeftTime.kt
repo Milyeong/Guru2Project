04f1496ea1ac7c1200a4e25073c9f2a953ad2d94
JSA <sooa3581@gmail.com>
2021-08-04 06:03:11 +0900

    xml 파일

--- app/src/main/java/com/example/guru2project/LeftTime.kt
+++ app/src/main/java/com/example/guru2project/LeftTime.kt
@@ -1,11 +1,86 @@
 package com.example.guru2project
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.usage.UsageEvents
+import android.app.usage.UsageStatsManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.HashMap
 
 class LeftTime : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_left_time)
+
+
+        var tz= TimeZone.getDefault()
+        var tzId= tz.toZoneId()
+        var dtNow = LocalDate.now()
+        var dtStart = dtNow.atStartOfDay(tzId).toInstant().toEpochMilli()
+
+        var usageList = queryUsageTimes(dtStart, System.currentTimeMillis())
+        usageList.forEach { it->
+            println(it.first+" : "+it.second)
+        }
+
+
+    }
+
+    //하루동안 사용한 앱의 패키지 이름과 사용시간을 MutableList로 가져오기
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun queryUsageTimes(startTime: Long, endTime: Long) : MutableList<Pair<String, Long>>{
+        var currentEvent: UsageEvents.Event
+        val allEvents: MutableList<UsageEvents.Event> = ArrayList()
+        val map: HashMap<String, Long> = HashMap()
+        val mUsageStatsManager = getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        // 발생 이벤트를 쿼리
+        val usageEvents = mUsageStatsManager.queryEvents(startTime, endTime)
+
+        while (usageEvents.hasNextEvent()) {
+            currentEvent = UsageEvents.Event()
+            usageEvents.getNextEvent(currentEvent)
+            val packageName = currentEvent.packageName
+            //추가 이벤트가 발견되면 이벤트 목록에 추가
+            if (currentEvent.eventType == UsageEvents.Event.ACTIVITY_RESUMED || currentEvent.eventType == UsageEvents.Event.ACTIVITY_PAUSED || currentEvent.eventType == UsageEvents.Event.ACTIVITY_STOPPED) {
+                allEvents.add(currentEvent)
+                if (!map.containsKey(packageName)) {
+                    map[packageName] = 0
+                }
+            }
+        }
+
+        //이벤트 발생시
+        for (i in 0 until (allEvents.size - 1) step 1) {
+            val event0 = allEvents[i]
+            val event1 = allEvents[i + 1]
+
+            //앱 실행할때마다 사용시간 측정
+            if (event0.eventType == UsageEvents.Event.ACTIVITY_RESUMED &&
+                (event1.eventType == UsageEvents.Event.ACTIVITY_PAUSED || event1.eventType == UsageEvents.Event.ACTIVITY_STOPPED)
+                && event0.packageName == event1.packageName) {
+                val runtime = event1.timeStamp - event0.timeStamp
+                val tInForeground = map[event0.packageName]!!.plus(runtime)
+                map[event0.packageName] = tInForeground
+            }
+        }
+
+        // 사용시간을 기준으로 내림차순으로 배열
+        val result = map.toList().sortedByDescending { (_, value) -> value}
+        val list:MutableList<Pair<String, Long>> = mutableListOf()
+        //하루동안 제일 많이 사용한 어플 3개
+        for (i in 0..2) {
+            list.add(Pair(result[i].first, result[i].second))
+
+            //print(result[i].first)
+            //println(result[i].second)
+        }
+        return list
     }
 }
--- app/src/main/java/com/example/guru2project/SettingTimeActivity.kt
+++ app/src/main/java/com/example/guru2project/SettingTimeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.guru2project
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Build
@@ -7,10 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
+import android.widget.*
 import androidx.annotation.RequiresApi
 
 class SettingTimeActivity : AppCompatActivity() {
@@ -22,16 +20,20 @@ class SettingTimeActivity : AppCompatActivity() {
     private lateinit var minuteSpinner: Spinner
     private lateinit var btnTimeSet: Button
 
+    /*val DEFAULT_INTERVAL = 5
+    val MINUTES_MIN = 0
+    val MINUTES_MAX = 60*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting_time)
 
-        //hourSpinner = findViewById(R.id.hour_spinner)
-        //minuteSpinner = findViewById(R.id.minute_spinner)
-        //btnTimeSet = findViewById(R.id.btnSetTime)
+        hourSpinner = findViewById(R.id.hour_spinner)
+        minuteSpinner = findViewById(R.id.minute_spinner)
+        btnTimeSet = findViewById(R.id.btnSetTime)
 
         //hourSpinner.setSelection(0) 원하는 포지션 이동(나중에 추가)
+
 
         //시간 선택
         hourSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
@@ -54,7 +56,6 @@ class SettingTimeActivity : AppCompatActivity() {
         //시간설정 확인
         btnTimeSet.setOnClickListener {
             var dlg = AlertDialog.Builder(this)
-            //dlg.setTitle("권한이 필요한 이유")
             dlg.setMessage("${setHour}시간 ${setMinute}분으로 설정하시겠습니까?")
             dlg.setPositiveButton( "확인") { dialog, which ->
                 startActivity(Intent(this, LeftTime::class.java))
@@ -65,10 +66,44 @@ class SettingTimeActivity : AppCompatActivity() {
 
         }
 
-
-
-
-
-
     }
+
+
+        /*@SuppressLint("PrivateApi")
+        fun TimePicker.setTimeInterval(
+                timeInterval: Int = DEFAULT_INTERVAL
+        ) {
+            try {
+                val classForId = Class.forName("com.android.internal.R\$id")
+                val fieldId = classForId.getField("minute").getInt(null)
+
+                (this.findViewById(fieldId) as NumberPicker).apply {
+                    minValue = MINUTES_MIN
+                    maxValue = MINUTES_MAX / timeInterval - 1
+                    displayedValues = getDisplayedValue(timeInterval)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+
+
+
+
+
+
+    private fun getDisplayedValue(timeInterval: Int = DEFAULT_INTERVAL): Array<String> {
+        val minutesArray = ArrayList<String>()
+        for (i in 0 until MINUTES_MAX step timeInterval) {
+            minutesArray.add(i.toString())
+        }
+
+        return minutesArray.toArray(arrayOf(""))
+    }
+
+    private fun TimePicker.getDisplayedMinute(
+
+            timeInterval: Int = DEFAULT_INTERVAL
+    ): Int = minute * timeInterval*/
 }
