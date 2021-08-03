97296888eb8cf9747d2d2807e112474742524598
JSA <sooa3581@gmail.com>
2021-08-04 07:38:25 +0900

    xml 파일

--- app/src/main/java/com/example/guru2project/LeftTime.kt
+++ app/src/main/java/com/example/guru2project/LeftTime.kt
@@ -5,15 +5,20 @@ import android.app.usage.UsageStatsManager
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import java.text.SimpleDateFormat
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import java.util.*
 import kotlin.collections.HashMap
 
 class LeftTime : AppCompatActivity() {
+
+    lateinit var usageMap: HashMap<String, Long>
+    lateinit var appName: TextView
+    lateinit var appUsageTime: TextView
+
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,17 +30,31 @@ class LeftTime : AppCompatActivity() {
         var dtNow = LocalDate.now()
         var dtStart = dtNow.atStartOfDay(tzId).toInstant().toEpochMilli()
 
-        var usageList = queryUsageTimes(dtStart, System.currentTimeMillis())
-        usageList.forEach { it->
-            println(it.first+" : "+it.second)
+        usageMap = mapUsageTimes(dtStart, System.currentTimeMillis())
+
+        var totalTime: Long = 0
+        usageMap.forEach {it->
+            totalTime += it.value
         }
+
+        // 사용시간을 기준으로 내림차순으로 배열
+        val result = usageMap.toList().sortedByDescending { (_, value) -> value}
+        val list:MutableList<Pair<String, Long>> = mutableListOf()
+        //하루동안 제일 많이 사용한 어플 3개
+        for (i in 0..2) {
+            list.add(Pair(result[i].first, result[i].second))
+
+                //print(result[i].first)
+                //println(result[i].second)
+        }
+
 
 
     }
 
     //하루동안 사용한 앱의 패키지 이름과 사용시간을 MutableList로 가져오기
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    private fun queryUsageTimes(startTime: Long, endTime: Long) : MutableList<Pair<String, Long>>{
+    private fun mapUsageTimes(startTime: Long, endTime: Long) : HashMap<String, Long>{
         var currentEvent: UsageEvents.Event
         val allEvents: MutableList<UsageEvents.Event> = ArrayList()
         val map: HashMap<String, Long> = HashMap()
@@ -71,16 +90,6 @@ class LeftTime : AppCompatActivity() {
             }
         }
 
-        // 사용시간을 기준으로 내림차순으로 배열
-        val result = map.toList().sortedByDescending { (_, value) -> value}
-        val list:MutableList<Pair<String, Long>> = mutableListOf()
-        //하루동안 제일 많이 사용한 어플 3개
-        for (i in 0..2) {
-            list.add(Pair(result[i].first, result[i].second))
-
-            //print(result[i].first)
-            //println(result[i].second)
-        }
-        return list
+        return map
     }
 }
